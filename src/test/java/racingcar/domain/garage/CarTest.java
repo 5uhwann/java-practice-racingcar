f5f64a0738b0ee3b5ae2c2fd62704c10d4ea2a85
5uhwann <5uhwann27@gmail.com>
2022-12-01 00:53:49 +0900

    test: 자동차 이름 입력 검증 테스트 작성

--- src/test/java/racingcar/domain/garage/CarTest.java
+++ src/test/java/racingcar/domain/garage/CarTest.java
@@ -10,11 +10,31 @@ import org.junit.jupiter.params.provider.ValueSource;
 class CarTest {
 
     @ParameterizedTest
+    @ValueSource(strings = {"hwan", "pobi" })
+    @DisplayName("5자 이하의 이름의 자동차는 생성 가능하다.")
+    void successMakeCarTest(String carName) {
+        //given //when
+        Car car = new Car(carName);
+
+        //then
+        assertThat(car.getName()).isEqualTo(carName);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"suhwan", "pobipobi" })
+    @DisplayName("5를 초과하의 이름의 자동차는 생성 불가능하다.")
+    void failMakeCarTest(String carName) {
+        //given //when //then
+        assertThatThrownBy(() -> new Car(carName)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+    @ParameterizedTest
     @ValueSource(ints = {3,7,2,4})
     @DisplayName("자동차는 받은 입력 숫자에 따른 이동을 한다.")
     void moveTest(int randomNumber) {
         //given
-        Car car = new Car("suhwan");
+        Car car = new Car("hwan");
 
         //when
         car.move(randomNumber);
