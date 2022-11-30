f9a867bbba24ee6fd0ba94ea205a462c39d0b1ac
5uhwann <5uhwann27@gmail.com>
2022-12-01 00:54:51 +0900

    feat: 자동차 이름 검증 구현

--- src/main/java/racingcar/domain/garage/Car.java
+++ src/main/java/racingcar/domain/garage/Car.java
@@ -3,10 +3,14 @@ package racingcar.domain.garage;
 import static racingcar.domain.operator.Command.move;
 
 public class Car {
+
+    private final static int MAX_NAME_SIZE = 5;
+
     private final String name;
     private int position = 0;
 
     public Car(String name) {
+        validateCarName(name);
         this.name = name;
     }
 
@@ -18,6 +22,13 @@ public class Car {
     public int getPosition() {
         return position;
     }
+
+    private void validateCarName(String name) {
+        if (name.length() > MAX_NAME_SIZE) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
+        }
+    }
+
 
     public void move(int randomNumber) {
         if (move.getConditions().contains(randomNumber)) {
